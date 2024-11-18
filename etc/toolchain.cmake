7b2e4f57b3d4d1fce0cc89240755756ee2b8426c
Claus Klein <claus.klein@rohde-schwarz.com>
2024-11-18 11:17:30 +0100

    Add support for cl compiler on windows

--- etc/toolchain.cmake
+++ etc/toolchain.cmake
@@ -1,6 +1,13 @@
 include_guard(GLOBAL)
 
-include("${CMAKE_CURRENT_LIST_DIR}/clang-flags.cmake")
+if(UNIX)
+    include("${CMAKE_CURRENT_LIST_DIR}/clang-flags.cmake")
 
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_CXX_COMPILER clang++)
+    set(CMAKE_C_COMPILER clang)
+    set(CMAKE_CXX_COMPILER clang++)
+else()
+    # include("${CMAKE_CURRENT_LIST_DIR}/cl-flags.cmake")
+
+    set(CMAKE_C_COMPILER cl)
+    set(CMAKE_CXX_COMPILER cl)
+endif()
